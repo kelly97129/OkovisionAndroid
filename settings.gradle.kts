d3886c9c0ecf248035c099b205fc046a22390f84
kelly97129 <fagotindavid@msn.com>
2025-12-01 22:39:59 +0100

    Initial commit Okovision Android

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,21 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        // Pour le plugin MPAndroidChart (JitPack)
+        maven("https://jitpack.io")
     }
 }
+
 dependencyResolutionManagement {
+    // Tu peux garder FAIL_ON_PROJECT_REPOS si tu veux quelque chose de strict
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // IMPORTANT : dépôt JitPack pour MPAndroidChart
+        maven("https://jitpack.io")
     }
 }
+
 rootProject.name = "OkovisionAndroid"
 include(":app")
